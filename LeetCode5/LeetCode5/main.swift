0e1a35e995cb81e66e1a45674d4e1984c2ee1b98
yangzai360 <jieyangdh@gmail.com>
2016-04-11 17:39:07 +0800

    Not finished yet.

--- LeetCode5/LeetCode5/main.swift
+++ LeetCode5/LeetCode5/main.swift
@@ -8,38 +8,24 @@
 
 import Foundation
 
-private extension String {
-    subscript (i: Int) -> Character {
-        let index: Index = self.startIndex.advancedBy(i)
-        return self[index]
-    }
-    subscript (range: Range<Int>) -> String {
-        return self[self.startIndex.advancedBy(range.startIndex)..<self.startIndex.advancedBy(range.endIndex, limit: self.endIndex)]
-    }
-}
-
 class Solution {
     var longestSubstringSP :Int = 0
     var longestNum :Int = 0
     
     func longestPalindrome(s: String) -> String {
-        
-        var i = 0
-        while i < s.characters.count{
-            i += 1
-        }
         for i in 0 ..< s.characters.count {
             findLongestPalindromeSubStringWithString(s, startIndex: i, endIndex: i+1)
             findLongestPalindromeSubStringWithString(s, startIndex: i, endIndex: i)
         }
-        return s[longestSubstringSP ..< longestSubstringSP + longestNum]
+//        var result = 
+        return (s as NSString).substringWithRange(NSMakeRange(longestSubstringSP, longestNum))
     }
     
     func findLongestPalindromeSubStringWithString(str :String, startIndex sp:Int, endIndex ep:Int){
-        
         var ssp = sp
         var eep = ep
-        while ssp >= 0  && eep < str.characters.count && str[ssp] == str[eep] {
+        let nss = NSString(string: str)
+        while ssp >= 0  && eep < nss.length && nss.characterAtIndex(ssp) == nss.characterAtIndex(eep) {
             ssp -= 1
             eep += 1
         }
@@ -51,10 +37,5 @@ class Solution {
 }
 
 let s = Solution()
-let time1 = NSDate()
-print(s.longestPalindrome("jrjnbctoqgzimtoklkxcknwmhiztomaofwwzjnhrijwkgmwwuazcowskjhitejnvtblqyepxispasrgvgzqlvrmvhxusiqqzzibcyhpnruhrgbzsmlsuacwptmzxuewnjzmwxbdzqyvsjzxiecsnkdibudtvthzlizralpaowsbakzconeuwwpsqynaxqmgngzpovauxsqgypinywwtmekzhhlzaeatbzryreuttgwfqmmpeywtvpssznkwhzuqewuqtfuflttjcxrhwexvtxjihunpywerkktbvlsyomkxuwrqqmbmzjbfytdddnkasmdyukawrzrnhdmaefzltddipcrhuchvdcoegamlfifzistnplqabtazunlelslicrkuuhosoyduhootlwsbtxautewkvnvlbtixkmxhngidxecehslqjpcdrtlqswmyghmwlttjecvbueswsixoxmymcepbmuwtzanmvujmalyghzkvtoxynyusbpzpolaplsgrunpfgdbbtvtkahqmmlbxzcfznvhxsiytlsxmmtqiudyjlnbkzvtbqdsknsrknsykqzucevgmmcoanilsyyklpbxqosoquolvytefhvozwtwcrmbnyijbammlzrgalrymyfpysbqpjwzirsfknnyseiujadovngogvptphuyzkrwgjqwdhtvgxnmxuheofplizpxijfytfabx"))
-//print(s.longestPalindrome("sdsdsdsss"))
-let time2 = NSDate()
-
-print(time2.timeIntervalSince1970 - time1.timeIntervalSince1970)
+print(s.longestPalindrome("abccbfcfbcqh"))
 
