c92e412e5dca2f84ad0ada5c4fbeab2796322a4a
jieyang <jieyangdh@gmail.com>
2016-04-13 00:25:35 +0800

    No.6 accepted.

--- LeetCode6/LeetCode6/main.swift
+++ LeetCode6/LeetCode6/main.swift
@@ -8,5 +8,30 @@
 
 import Foundation
 
-println("Hello, World!")
+class Solution {
+  func convert(s: String, _ numRows: Int) -> String {
+    if numRows == 1 {
+      return s
+    }
+    var strArray = [String](count: numRows, repeatedValue: "")
+    var direct = true
+    var lineMark = 0
+    for char in s.characters {
+      strArray[lineMark].append(char)
+      if (lineMark == numRows - 1  && direct) || (lineMark == 0  && !direct) {
+        direct = !direct
+      }
+      lineMark += direct ? 1 : -1
+    }
+    var resultStr = ""
+    for str in strArray {
+      resultStr += str
+    }
+    return resultStr
+  }
+}
 
+
+let so = Solution()
+print(so.convert("1234567890123456789", 5))
+print(so.convert("AB", 1))
