212d8a0962c68d5635507d143f112ec52d3f3434
jieyang <jieyangdh@gmail.com>
2016-04-13 00:25:06 +0800

    No.6 accepted.

---
+++ LeetCode6/LeetCode6/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LeetCode6
+//
+//  Created by jieyang on 16/4/12.
+//  Copyright (c) 2016年 Sean.Jie. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
